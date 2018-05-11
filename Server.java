caa26ff0a6a1fb55b8ab261fdc492801cf203ddb
Conor Ryan <37616617+conorjcryan@users.noreply.github.com>
2018-05-11 13:39:34 +0100

    updates
    removed hard coded port numbers and ip address

--- Server.java
+++ Server.java
@@ -1,11 +1,14 @@
-/*******************************************************************************
- * Conor Ryan - Socket Programming - Java				       *
- * Basic UDP Server							       *
- * April 2018			       				               *
- *******************************************************************************/
+/*********************************************
+ * Conor Ryan - Socket Programming - Java    *
+ * Basic UDP Server                          *
+ * April 2018                                *
+ *********************************************/
 
-package sockets;
+package testtest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
@@ -13,26 +16,41 @@ public class Server
 {
 	public static void main(String[] args) throws Exception
 	{
-			//Create new datagram server socket "servsock" and pass it port number
-			DatagramSocket servsock = new DatagramSocket(9999);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String temp;
+		int port = 0;
+		
+		System.out.print("Please enter port: ");
+    	try 
+    	{
+        	temp = reader.readLine();
+        	port = Integer.parseInt(temp);
+    	} 
+    	catch (IOException e) 
+    	{
+    		e.printStackTrace();
+    	}
+		
+		//Create new datagram server socket "servsock" and pass it port number
+    	DatagramSocket socket = new DatagramSocket(port);
 			
-			//Create blank array for incoming data
-			byte[] blankbyte = new byte[1024];
+    	//Create blank array for incoming data
+    	byte[] blank = new byte[1024];
 			
-			//Create a new packet "incomingpacket" for incoming data
-			DatagramPacket incomingpacket = new DatagramPacket(blankbyte,blankbyte.length);
-			
-			//Recieve "incomingpacket" from the socket
-			servsock.receive(incomingpacket);
-			
-			//Convert the byte array "bytes" into a string
-			String data = new String(incomingpacket.getData());
-			
-			//Display the data receieved to the user
-			System.out.println("Client Sent: " + data);
-			
-			//Close the server socket "servsock"
-			servsock.close();
+		//Create a new packet "incomingpacket" for incoming data
+		DatagramPacket packet = new DatagramPacket(blank,blank.length);
+		
+		//Recieve "incomingpacket" from the socket
+		socket.receive(packet);
+		
+		//Convert the byte array "bytes" into a string
+		String data = new String(packet.getData());
+		
+		//Display the data receieved to the user
+		System.out.println("Message received: " + data);
+		
+		//Close the server socket "servsock"
+		socket.close();
 
 	}
 }
